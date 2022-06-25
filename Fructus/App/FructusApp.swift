23a2ed879ff52d8de49670633fd3517a9d196eb4
Gitanshu1903 <gpvaghasiya1903@gmail.com>
2022-06-25 12:02:27 +0530

    Initial Commit

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by ERASOFT on 11/04/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            OnboardingView()
+        }
+    }
+}
